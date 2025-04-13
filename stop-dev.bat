3b7db6cfb2bc816ade74548d1a70aa60b2810e32
SidSpanos <128487361+SidSpanos@users.noreply.github.com>
2025-04-13 02:41:24 +0200

    Delete stop-dev.bat

--- stop-dev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Stopping LCARS Timeline Development Environment...
-
-:: Kill any running node processes
-taskkill /F /IM node.exe
-taskkill /F /IM cmd.exe /FI "WINDOWTITLE eq SASS Compiler"
-taskkill /F /IM cmd.exe /FI "WINDOWTITLE eq Webpack"
-
-echo Development environment stopped. 
