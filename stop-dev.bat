3d1072f1b5412f29dac95d872ca841473ccddba6
SidSpanos <128487361+SidSpanos@users.noreply.github.com>
2025-04-13 02:59:12 +0200

    Remove batch files from git tracking, update .gitignore to exclude .bat files

--- stop-dev.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping LCARS Timeline Development Environment...
-taskkill /F /IM node.exe
-taskkill /F /IM cmd.exe /FI "WINDOWTITLE eq SASS Compiler"
-taskkill /F /IM cmd.exe /FI "WINDOWTITLE eq Webpack"
-echo Development environment stopped. 
